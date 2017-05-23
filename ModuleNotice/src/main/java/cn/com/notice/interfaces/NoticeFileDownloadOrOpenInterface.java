4ea14761ecd862777477a77f0076faffee3b5a84
志强 <zzqtdos@gmail.com>
2017-05-23 17:23:29 +0800

    通知公告模块,修改通知详情的文件展示和下载

---
+++ ModuleNotice/src/main/java/cn/com/notice/interfaces/NoticeFileDownloadOrOpenInterface.java
@@ -0,0 +1,9 @@
+package cn.com.notice.interfaces;
+
+/**
+ * Created by 志强 on 2017.5.23.
+ */
+
+public interface NoticeFileDownloadOrOpenInterface {
+    void fileDownLoadOrOpenInterface(int position);
+}
