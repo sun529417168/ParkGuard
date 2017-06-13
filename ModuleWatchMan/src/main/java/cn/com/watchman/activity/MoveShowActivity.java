d071bbe4025f6091545cdb2392215a5ed40e61bf
529417168@qq.com <xue7758521>
2017-06-13 17:17:24 +0800

    1.轨迹地图加入回到当前位置

--- ModuleWatchMan/src/main/java/cn/com/watchman/activity/MoveShowActivity.java
+++ ModuleWatchMan/src/main/java/cn/com/watchman/activity/MoveShowActivity.java
@@ -1,11 +1,15 @@
 package cn.com.watchman.activity;
 
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Pair;
 import android.view.View;
 
 import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
 import com.amap.api.location.AMapLocationListener;
 import com.amap.api.maps.AMap;
 import com.amap.api.maps.CameraUpdateFactory;
@@ -13,10 +17,15 @@ import com.amap.api.maps.LocationSource;
 import com.amap.api.maps.MapView;
 import com.amap.api.maps.model.BitmapDescriptor;
 import com.amap.api.maps.model.BitmapDescriptorFactory;
+import com.amap.api.maps.model.Circle;
+import com.amap.api.maps.model.CircleOptions;
 import com.amap.api.maps.model.LatLng;
 import com.amap.api.maps.model.LatLngBounds;
+import com.amap.api.maps.model.Marker;
+import com.amap.api.maps.model.MarkerOptions;
 import com.amap.api.maps.model.PolylineOptions;
 import com.linked.erfli.library.base.BaseActivity;
+import com.linked.erfli.library.utils.ToastUtil;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,7 +35,9 @@ import cn.com.watchman.R;
 import cn.com.watchman.bean.DinatesBean;
 import cn.com.watchman.bean.DinatesDaoImpl;
 import cn.com.watchman.utils.PointsUtil;
+import cn.com.watchman.utils.SensorEventHelper;
 import cn.com.watchman.utils.SmoothMarker;
+import cn.com.watchman.utils.WMyUtils;
 
 /**
  * 文件名：MoveShowActivity
@@ -40,6 +51,16 @@ public class MoveShowActivity extends BaseActivity implements LocationSource, AM
     private MapView mapView;
     private AMap aMap;
     private DinatesDaoImpl dinatesDao;
+    private SensorEventHelper mSensorHelper;
+    private boolean mFirstFix = false;
+    private Marker mLocMarker;
+    private AMapLocationClient mlocationClient;
+    private AMapLocationClientOption mLocationOption;
+    private OnLocationChangedListener mListener;
+    private Circle mCircle;
+    private static final int STROKE_COLOR = Color.argb(180, 3, 145, 255);
+    private static final int FILL_COLOR = Color.argb(10, 0, 0, 180);
+    public static final String LOCATION_MARKER_FLAG = "mylocation";
 
     @Override
     protected void setView() {
@@ -102,27 +123,43 @@ public class MoveShowActivity extends BaseActivity implements LocationSource, AM
     }
 
     private List<LatLng> readLatLngs() {
-        List<LatLng> points = new ArrayList<LatLng>();
-        List<DinatesBean> dinatesList = dinatesDao.find();
+        List<LatLng> points = new ArrayList<>();
+        List<DinatesBean> dinatesList = dinatesDao.rawQuery("select * from t_gps where time > ?", new String[]{WMyUtils.getTimesmorning()});
         for (DinatesBean bean : dinatesList) {
             points.add(new LatLng(bean.getLatitude(), bean.getLongitude()));
         }
         return points;
     }
 
-    public void onBackClice(View view) {
+    public void onBackClick(View view) {//定位
         setUpMap();
+        mSensorHelper = new SensorEventHelper(this);
+        if (mSensorHelper != null) {
+            mSensorHelper.registerSensorListener();
+        }
     }
+
+    public void onTrajectory(View view) {//轨迹
+        mListener = null;
+        if (mlocationClient != null) {
+            mlocationClient.stopLocation();
+            mlocationClient.onDestroy();
+        }
+        mlocationClient = null;
+        init();
+    }
+
     /**
      * 设置一些amap的属性
      */
     private void setUpMap() {
         aMap.setLocationSource(this);// 设置定位监听
-        aMap.getUiSettings().setMyLocationButtonEnabled(true);// 设置默认定位按钮是否显示
+        aMap.getUiSettings().setMyLocationButtonEnabled(false);// 设置默认定位按钮是否显示
         aMap.setMyLocationEnabled(true);// 设置为true表示显示定位层并可触发定位，false表示隐藏定位层并不可触发定位，默认是false
         // 设置定位的类型为定位模式 ，可以由定位、跟随或地图根据面向方向旋转几种
         aMap.setMyLocationType(AMap.LOCATION_TYPE_LOCATE);
     }
+
     /**
      * 方法必须重写
      */
@@ -130,7 +167,9 @@ public class MoveShowActivity extends BaseActivity implements LocationSource, AM
     protected void onResume() {
         super.onResume();
         mapView.onResume();
-
+        if (mSensorHelper != null) {
+            mSensorHelper.registerSensorListener();
+        }
     }
 
     /**
@@ -140,7 +179,14 @@ public class MoveShowActivity extends BaseActivity implements LocationSource, AM
     protected void onPause() {
         super.onPause();
         mapView.onPause();
-
+        if (mSensorHelper != null) {
+            mSensorHelper.unRegisterSensorListener();
+            mSensorHelper.setCurrentMarker(null);
+            mSensorHelper = null;
+        }
+        mapView.onPause();
+        deactivate();
+        mFirstFix = false;
     }
 
     /**
@@ -159,21 +205,96 @@ public class MoveShowActivity extends BaseActivity implements LocationSource, AM
     protected void onDestroy() {
         super.onDestroy();
         mapView.onDestroy();
-
+        if (mLocMarker != null) {
+            mLocMarker.destroy();
+        }
+        if (null != mlocationClient) {
+            mlocationClient.onDestroy();
+        }
     }
 
+    /**
+     * 定位成功后回调函数
+     */
     @Override
     public void onLocationChanged(AMapLocation aMapLocation) {
-
+        if (mListener != null && aMapLocation != null) {
+            if (aMapLocation != null && aMapLocation.getErrorCode() == 0) {
+                LatLng location = new LatLng(aMapLocation.getLatitude(), aMapLocation.getLongitude());
+                if (!mFirstFix) {
+                    mFirstFix = true;
+                    addCircle(location, aMapLocation.getAccuracy());//添加定位精度圆
+                    addMarker(location);//添加定位图标
+                    mSensorHelper.setCurrentMarker(mLocMarker);//定位图标旋转
+                    aMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 18));
+                } else {
+                    mCircle.setCenter(location);
+                    mCircle.setRadius(aMapLocation.getAccuracy());
+                    mLocMarker.setPosition(location);
+                    aMap.moveCamera(CameraUpdateFactory.changeLatLng(location));
+                }
+            } else {
+                String errText = "定位失败," + aMapLocation.getErrorCode() + ": " + aMapLocation.getErrorInfo();
+                Log.e("AmapErr", errText);
+            }
+        }
     }
 
+    /**
+     * 激活定位
+     */
     @Override
     public void activate(OnLocationChangedListener onLocationChangedListener) {
-
+        mListener = onLocationChangedListener;
+        if (mlocationClient == null) {
+            mlocationClient = new AMapLocationClient(this);
+            mLocationOption = new AMapLocationClientOption();
+            //设置定位监听
+            mlocationClient.setLocationListener(this);
+            //设置为高精度定位模式
+            mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+            //设置定位参数
+            mlocationClient.setLocationOption(mLocationOption);
+            // 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
+            // 注意设置合适的定位时间的间隔（最小间隔支持为2000ms），并且在合适时间调用stopLocation()方法来取消定位请求
+            // 在定位结束后，在合适的生命周期调用onDestroy()方法
+            // 在单次定位情况下，定位无论成功与否，都无需调用stopLocation()方法移除请求，定位sdk内部会移除
+            mlocationClient.startLocation();
+        }
     }
 
+    /**
+     * 停止定位
+     */
     @Override
     public void deactivate() {
+        mListener = null;
+        if (mlocationClient != null) {
+            mlocationClient.stopLocation();
+            mlocationClient.onDestroy();
+        }
+        mlocationClient = null;
+    }
 
+    private void addCircle(LatLng latlng, double radius) {
+        CircleOptions options = new CircleOptions();
+        options.strokeWidth(1f);
+        options.fillColor(FILL_COLOR);
+        options.strokeColor(STROKE_COLOR);
+        options.center(latlng);
+        options.radius(radius);
+        mCircle = aMap.addCircle(options);
+    }
+
+    private void addMarker(LatLng latlng) {
+        if (mLocMarker != null) {
+            return;
+        }
+        MarkerOptions options = new MarkerOptions();
+        options.icon(BitmapDescriptorFactory.fromBitmap(BitmapFactory.decodeResource(this.getResources(), R.mipmap.navi_map_gps_locked)));
+        options.anchor(0.5f, 0.5f);
+        options.position(latlng);
+        mLocMarker = aMap.addMarker(options);
+        mLocMarker.setTitle(LOCATION_MARKER_FLAG);
     }
 }
